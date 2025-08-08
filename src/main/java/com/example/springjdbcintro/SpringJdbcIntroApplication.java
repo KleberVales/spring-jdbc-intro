cd1764b38da7dc22b9b3324e24ed23c37bf5eb89
Kleber Vales <klebervales.dev@gmail.com>
2025-08-08 11:11:14 -0300

    Initial Commit

---
+++ src/main/java/com/example/springjdbcintro/SpringJdbcIntroApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjdbcintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcIntroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJdbcIntroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springjdbcintro/SpringJdbcIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjdbcintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbcIntroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
